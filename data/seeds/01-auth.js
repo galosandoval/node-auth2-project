8d73a0e07007a91aaf8fc056f24a64db9dc60797
Galo Sandoval <galosan92@gmail.com>
2020-09-15 15:39:05 -0700

    added migration and seeds

--- data/seeds/01-auth.js
+++ data/seeds/01-auth.js
@@ -1,13 +1,17 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+  const users = [
+    {
+      username: "test",
+      department: "testing",
+      password: "password",
+    },
+    {
+      username: "test2",
+      department: "testing",
+      password: "password",
+    },
+  ];
+  return knex("users")
+    .insert(users)
+    .then(() => console.log("== Seed data for roles table added. =="));
 };
