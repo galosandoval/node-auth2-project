664d9080cd6179e2381370784f9503d22de94577
Galo Sandoval <galosan92@gmail.com>
2020-09-15 15:20:54 -0700

    added migration seed files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+// const authRouter = require("../auth/auth-router.js");
+// const usersRouter = require("../users/users-router.js");
+// const restricted = require('../auth/restricted-middleware')
+// const checkRole = require("../auth/check-role-middleware")
+
+const server = express();
+
+server.use(express.json());
+server.use(
+  cors({
+    origin: "*",
+    credentials: true, // works in tandem with the withCredentials option
+  })
+);
+
+// server.use("/api/auth", authRouter);
+// server.use("/api/users", usersRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
